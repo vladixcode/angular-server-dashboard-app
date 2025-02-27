ea1a8c5bf47addb646df2faddab3206b5b9fcd5b
Vladimir Ilic <vladimir.ilic101@gmail.com>
2025-02-27 14:53:48 +0100

    Component & Templates features - Lifecycle hooks; Template variables; ViewChild & ContentChild; Signals & Effects; etc

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,15 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core'
+import {
+  AfterContentInit,
+  afterNextRender,
+  afterRender,
+  Component,
+  contentChild,
+  // ContentChild,
+  ElementRef,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core'
 
 // import { HostBinding, HostListener } from '@angular/core'
 
@@ -14,7 +25,7 @@ import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angula
     '(click)': 'onClick()',
   },
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   // @HostBinding('class') className = 'control'
 
   // @HostListener('click') onClick() {
@@ -24,8 +35,30 @@ export class ControlComponent {
 
   private hostElement = inject(ElementRef)
 
+  // @ContentChild('inputEl') private controlElement?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >
+
+  constructor() {
+    afterRender(() => {
+      console.log('After Render')
+    })
+
+    afterNextRender(() => {
+      console.log('After Next Render')
+    })
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.controlElement)
+  }
+
+  private controlElement =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('inputEl')
+
   onClick() {
     console.log('click')
     console.log(this.hostElement)
+    console.log(this.controlElement())
   }
 }
